12a908f8ec0d20ca08d325b9dc8f9d82bbd12708
Cauã Mattos <55816531+spikeleez@users.noreply.github.com>
2024-03-24 04:21:40 -0300

    Create Attribute Info Data Asset

--- Source/Aura/Private/Data/AuraAttributeData.cpp
+++ Source/Aura/Private/Data/AuraAttributeData.cpp
@@ -3,3 +3,20 @@
 
 #include "Data/AuraAttributeData.h"
 
+FAuraAttributeInfo UAuraAttributeData::FindAttributeInfoByTag(const FGameplayTag& AttributeTag, const bool bLogNotFound) const
+{
+	for (const FAuraAttributeInfo& Info : AttributeInfo)
+	{
+		if (Info.AttributeTag.MatchesTagExact(AttributeTag))
+		{
+			return Info;
+		}
+	}
+	
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Can't find Info for AttributeTag [%s] on AttributeInfo [%s]."), *AttributeTag.ToString(), *GetNameSafe(this));
+	}
+
+	return FAuraAttributeInfo();
+}
--- Source/Aura/Public/Data/AuraAttributeData.h
+++ Source/Aura/Public/Data/AuraAttributeData.h
@@ -2,16 +2,39 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
 #include "Engine/DataAsset.h"
+#include "GameplayTagContainer.h"
 #include "AuraAttributeData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAuraAttributeInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FGameplayTag AttributeTag = FGameplayTag();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FText AttributeName = FText();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FText AttributeDescription = FText();
+
+	UPROPERTY(BlueprintReadOnly)
+	float AttributeValue = 0.f;
+};
 
 /**
  * 
  */
 UCLASS()
-class AURA_API UAuraAttributeData : public UDataAsset
+class UAuraAttributeData : public UDataAsset
 {
 	GENERATED_BODY()
+
+public:
+	FAuraAttributeInfo FindAttributeInfoByTag(const FGameplayTag& AttributeTag, const bool bLogNotFound = false) const;
 	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TArray<FAuraAttributeInfo> AttributeInfo;
 };
